223921eec43fc229a3e435831c0f9a1c4b15224a
Dsquare <darjidhaval19@gmail.com>
2021-03-25 22:07:52 +0530

    role static temporary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { LoginService } from "../services/login.service";
+import { LoginService } from '../services/login.service';
 import { Router } from '@angular/router';
 import { window } from 'rxjs/operators';
 import { ConfigurationModel } from './common/constant';
@@ -9,22 +9,18 @@ import { Login } from './models/login';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [
-    './app.component.css'
-  ],
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'gecp-admin';
   role: any;
 
-  constructor(private loginService: LoginService,
-    private router: Router) {
-
-  }
+  constructor(private loginService: LoginService, private router: Router) {}
 
   ngOnInit() {
     const userData = this.loginService.getDecodedAccessToken() as Login;
-    this.role = userData.role;
+    // this.role = userData.role;
+    this.role = 'Admin';
   }
 
   get getIsAuthorized() {
